103fe19002cd5a7be2f33f727a115d0f8fd76bf3
Thiago Magano <thiagomagano1993@outlook.com>
2025-10-29 20:01:56 -0300

    adicionado tasks no arquivo, caminho feliz

--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
 type Task struct {
-	id     int
-	title  string
-	status string
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
 }
 
-//tasksList := make[]
+const DB_PATH = "db.json" //Caminho para o arquivo
 
-func add(task string) string {
-	// Ler arquivo
-	// Json to Struct []hash
-	// Escrever no arquivo -> task
-	// retornar mensagem de sucesso
-	newTask := new(Task)
-	newTask.id = 1
-	newTask.title = task
-	newTask.status = "todo"
+// Ler -> Modificar -> Sobrescrever
 
-	return fmt.Sprintf("A tarefa \"%s\" foi adicionada com sucesso: (%d)", newTask.title, newTask.id)
+func leEescreveNoJson(path string, task string) (int, error) {
+	var tasks []Task
+
+	//Caso arquivo não exista, cria vazio
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		tasks = []Task{}
+	} else {
+		// Le os arquivos e coloca num slice array
+		data, _ := os.ReadFile(path)
+		json.Unmarshal(data, &tasks)
+	}
+
+	// adiciona nova tarefa
+	newTask := Task{ID: len(tasks) + 1, Title: task, Status: "todo"}
+	tasks = append(tasks, newTask)
+
+	// Salva tarefa no 'banco'
+	data, _ := json.MarshalIndent(tasks, "", "  ")
+	os.WriteFile(path, data, 0644)
+
+	return newTask.ID, nil
+}
+
+func add(title string) string {
+	id, err := leEescreveNoJson(DB_PATH, title)
+
+	if err != nil {
+		fmt.Printf("Erro encontrado: %s", err)
+	}
+
+	return fmt.Sprintf("Tarefa adicionada com sucesso ID: %d", id)
+
 }
 
 func main() {
@@ -37,7 +61,7 @@ func main() {
 	if args[0] == "add" {
 		if args[1] != "" {
 			msg := add(args[1])
-			fmt.Printf(msg)
+			fmt.Println(msg)
 		}
 	}
 }
